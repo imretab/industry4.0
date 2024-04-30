0320185d89ef14cefaea816fa215ce8ab91cd00a
imretab <imre.tabajdi@gmail.com>
2024-04-30 23:59:37 +0200

    Update putto_statistics.py
    New:
    - Added Figure and Canvas packages
    - Added title for the GUI
    - The figure shows the statistics within the window, not in a seperate window
    Fixed issues:
    - If the Figure window wasn't closed, the statistics overlapped each other (when used pyplot)
    - If the selected date was over the current date, it gave an error

--- putto_statistics.py
+++ putto_statistics.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 from tkinter import *
 from tkcalendar import Calendar
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 root = Tk()
+root.title("Putto statistics")
 root.geometry("1024x768")
 cal = Calendar(root,selectmode='day',date_pattern='y. mm. dd.')
 cal.pack(pady = 20)
@@ -26,6 +28,11 @@ def showStats():
         r = connection.post(main_url,data=data)
         soup = BeautifulSoup(r.content,'html.parser')
         table = soup.find('table')
+        if table is None:
+            fig.suptitle("Results: none")
+            canvas.draw()
+            canvas.get_tk_widget().pack()
+            return
         headings = [th.get_text().strip() for th in table.find("tr").find_all("th")]
         tbody = soup.find('tbody')
         datasets = []
@@ -36,23 +43,29 @@ def showStats():
         renameColums = dataFrame.rename(columns={0:headings[0],1:headings[1],2:headings[2],3:headings[3]})
         renameColums.to_csv('huzasok.csv',sep=';',encoding='utf-8')
         df = pd.read_csv('huzasok.csv',sep=';')
-        a_field_count = {}
+        a_field_counts = {}
         for index, row in df.iterrows():
             list_str = str(row.iloc[3])
             list_data = [int(x.strip()) for x in list_str.split(',')]
             for item in list_data:
-                a_field_count[item] = a_field_count.get(item,0)+1
+                a_field_counts[item] = a_field_counts.get(item,0)+1
         b_field_counts = df['"B" mező'].value_counts()
-        a_sorted = dict(sorted(a_field_count.items()))
+        a_sorted = dict(sorted(a_field_counts.items()))
         b_sorted = dict(sorted(b_field_counts.items()))
-        plt.subplot(1,2,1)
-        plt.bar(range(len(a_sorted)),list(a_sorted.values()))
-        plt.xticks(range(len(a_sorted)), list(a_sorted.keys()),size=7)
-        plt.title('Draws in field "A"',size=10)
-        plt.subplot(1,2,2)
-        plt.title('Draws in field "B"',size=10)
-        plt.bar(range(len(b_sorted)),list(b_sorted.values()))
-        plt.xticks(range(len(b_sorted)), list(b_sorted.keys()),size=7)
-        plt.show()
+        plotA.clear()
+        plotB.clear()
+        fig.suptitle("Statistics for {}".format(cal.get_date()))
+        plotA.bar(range(len(a_sorted)),list(a_sorted.values()))
+        plotA.set_title('Draws in field "A"',size=10)
+        plotA.set_xticks(range(len(a_sorted)), list(a_sorted.keys()),size=7)
+        plotB.bar(range(len(b_sorted)),list(b_sorted.values()))
+        plotB.set_title('Draws in field "B"',size=10)
+        plotB.set_xticks(range(len(b_sorted)), list(b_sorted.keys()),size=7)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+fig = Figure(figsize=(10,14),dpi=100)
+canvas = FigureCanvasTkAgg(fig, master=root)
+plotA = fig.add_subplot(121)
+plotB = fig.add_subplot(122)
 Button(root,text="Get statistics for selected date",command=showStats).pack(pady=20)
 root.mainloop()
